f869622ce73a910f500632061067ef02d8b1d138
Vinicius Farias <viniciusfariasb@gmail.com>
2021-02-12 10:23:01 -0300

    Implementação dos dados por usuário

--- pages/api/invocador/[...regiaoNome].js
+++ pages/api/invocador/[...regiaoNome].js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
 
     dataInvocador.championMastery = dataCampeoes;
 
-    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+    res.setHeader('Cache-Control', 's-maxage=7200, stale-while-revalidate');
     res.statusCode = 200;
     res.json({dataInvocador});
 
