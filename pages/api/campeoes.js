15417646b50a855812af49db1abf6aa515bbcebf
Vinicius Farias <viniciusfariasb@gmail.com>
2021-02-12 09:16:09 -0300

    Correção do retorno de campeões - #3

--- pages/api/campeoes.js
+++ pages/api/campeoes.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
     const lastVersion = data.splice(0, 1);
 
-    const responseChampions = await fetch(`http://ddragon.leagueoflegends.com/cdn/${lastVersion}/data/en_US/champion.json`);
+    const responseChampions = await fetch(`http://ddragon.leagueoflegends.com/cdn/${lastVersion}/data/pt_BR/champion.json`);
     const dataCampeoes = await responseChampions.json();
 
     res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate')
