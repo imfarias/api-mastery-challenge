3c448778d7359706d3fda9817fc36ecdca31bcad
Vinicius Farias <viniciusfariasb@gmail.com>
2021-02-12 09:07:45 -0300

    Commit inicial do endpoint de campeões - #1

---
+++ pages/api/campeoes.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+
+    const responseVersions = await fetch('https://ddragon.leagueoflegends.com/api/versions.json');
+    const data = await responseVersions.json();
+
+    const lastVersion = data.splice(0, 1);
+
+    const responseChampions = await fetch(`http://ddragon.leagueoflegends.com/cdn/${lastVersion}/data/en_US/champion.json`);
+    const dataCampeoes = await responseChampions.json();
+
+    res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate')
+    res.statusCode = 200
+    res.json({campeoes: dataCampeoes});
+}
